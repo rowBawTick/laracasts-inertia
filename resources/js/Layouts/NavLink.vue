82cc83ac2e827eda7a405c4c94f97afc06910151
chris <chris@substantiveresearch.com>
2023-02-15 17:56:08 +0000

    master: Finished lesson 8 Created a NavLink component to wrap the link component. Used Tailwind directives to dry up css.

---
+++ resources/js/Layouts/NavLink.vue
@@ -0,0 +1,23 @@
+<template>
+    <Link
+        class="link"
+        :class="{ 'link--active': active }"
+    >
+    <!-- slot is just a string, e.g. Home, which we could've passed in as a prop -->
+    <slot />
+    </Link>
+</template>
+
+<script>
+export default {
+    name: "NavLink",
+    props: {
+        active: Boolean,
+        // href: String,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Layouts/Navbar.vue
+++ resources/js/Layouts/Navbar.vue
@@ -1,8 +1,10 @@
 <script>
 import { Link } from '@inertiajs/inertia-vue3'
+import NavLink from "@/js/Layouts/NavLink.vue";
+
 export default {
     name: "Navbar",
-    components: { Link },
+    components: { Link, NavLink },
 }
 </script>
 
@@ -13,14 +15,22 @@ export default {
     <div class="flex items-center">
         <nav>
             <ul class="flex list-disc space-x-4 list-inside">
-                <li><Link class="text-blue-500 hover:underline" href="/">Home</Link></li>
-                <li><Link class="text-blue-500 hover:underline" href="/users">Users</Link></li>
-                <li><Link class="text-blue-500 hover:underline" href="/settings">Settings</Link></li>
+                <li>
+                    <NavLink href="/" :active="$page.url === '/'">Home</NavLink>
+                </li>
+                <li>
+                    <!-- If using query strings then can check the start -->
+                    <NavLink href="/users?foo=bar" :active="$page.url.startsWith('/users')">Users</NavLink>
+                </li>
+                <li>
+                    <!-- Can also match against the page component -->
+                    <NavLink href="/settings?foo=bar" :active="$page.component === 'Settings'">Settings</NavLink>
+                </li>
                 <!-- KU Share: Instead of creating a form and listening for it to be submitted -->
                 <!-- Can just have a button and handle it in web.php.
                     Inertia makes an axios request and sorts cookies for you too -->
                 <li>
-                    <Link class="text-blue-500 hover:underline"
+                    <Link class="link"
                           href="/logout"
                           method="post"
                           as="button"
